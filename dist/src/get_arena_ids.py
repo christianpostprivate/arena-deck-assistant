436635c382da7ac98b2731b103354a1467d1f7f9
MattR0se <37417239+MattR0se@users.noreply.github.com>
2020-03-20 21:33:31 +0100

    added windows executable
    also moved settings to json file

---
+++ dist/src/get_arena_ids.py
@@ -0,0 +1,106 @@
+import json
+from os import path, listdir
+import re
+
+# grpid = Arena card id
+# titleId = localisation id
+
+base_dir = path.dirname(path.abspath(__file__))
+data_folder = path.join(base_dir, '..', 'data')
+
+def get_names_from_id(data_cards_file, data_loc_file):
+    try:
+        with open(data_cards_file, 'r', encoding='utf-8') as f:
+            data_cards = json.load(f)
+
+        with open(data_loc_file, 'r', encoding='utf-8') as f:
+            data_loc = json.load(f)
+    except FileNotFoundError:
+        print('Can not find card data files.')
+        return
+
+    # connect card id with names
+    card_name_by_id = {}
+    for data_dict in data_loc:
+        if data_dict['isoCode'] == 'en-US':
+            for id_text in data_dict['keys']:
+                card_name_by_id[id_text['id']] = id_text['text']
+
+    card_ids_by_name = {}
+    for card in data_cards:
+        name = card_name_by_id[card['titleId']]
+        if name in card_ids_by_name.keys():
+            card_ids_by_name[name].append(card[str('grpid')])
+        else:
+            card_ids_by_name[name] = [card[str('grpid')]]
+
+    return card_ids_by_name
+
+
+def get_ids_from_name(data_cards_file, data_loc_file):
+    try:
+        with open(data_cards_file, 'r', encoding='utf-8') as f:
+            data_cards = json.load(f)
+
+        with open(data_loc_file, 'r', encoding='utf-8') as f:
+            data_loc = json.load(f)
+    except FileNotFoundError:
+        print('Can not find card data files.')
+        return
+
+    card_name_by_title_id = {}
+    for data_dict in data_loc:
+        if data_dict['isoCode'] == 'en-US':
+            for id_text in data_dict['keys']:
+                card_name_by_title_id[id_text['id']] = id_text['text']
+
+    card_name_by_id = {}
+    for card in data_cards:
+        name = card_name_by_title_id[card['titleId']]
+        arena_id = card['grpid']
+        card_name_by_id[arena_id] = name
+
+    return card_name_by_id
+
+
+def collect_arena_ids(arena_directory):
+    arena_directory += '\MTGA_Data\Downloads\Data'
+    files = listdir(arena_directory)
+    r_cards = re.compile(r'^data_cards.*.mtga$')
+    r_loc = re.compile(r'^data_loc.*.mtga$')
+    data_cards_file = (arena_directory + '\\' +
+                       list(filter(r_cards.match, files))[0])
+    data_loc_file = (arena_directory + '\\' +
+                     list(filter(r_loc.match, files))[0])
+
+    d = get_names_from_id(data_cards_file, data_loc_file)
+    with open(path.join(data_folder, 'card_id_by_name.json'), 'w',
+              encoding='utf-8') as f:
+        json.dump(d, f)
+
+    d = get_ids_from_name(data_cards_file, data_loc_file)
+    with open(path.join(data_folder, 'card_name_by_id.json'), 'w',
+              encoding='utf-8') as f:
+        json.dump(d, f)
+
+
+if __name__ == '__main__':
+    # for testing
+    ARENA_DIRECTORY = 'C:\Program Files (x86)\Wizards of the Coast\MTGA'
+    ARENA_DIRECTORY += '\MTGA_Data\Downloads\Data'
+
+    files = listdir(ARENA_DIRECTORY)
+
+    r_cards = re.compile(r'^data_cards.*.mtga$')
+    r_loc = re.compile(r'^data_loc.*.mtga$')
+
+    DATA_CARDS_FILE = ARENA_DIRECTORY + '\\' + list(filter(r_cards.match, files))[0]
+    DATA_LOC_FILE = ARENA_DIRECTORY + '\\' + list(filter(r_loc.match, files))[0]
+    d = get_names_from_id(DATA_CARDS_FILE, DATA_LOC_FILE)
+    with open(path.join(data_folder, 'card_id_by_name.json'), 'w', encoding='utf-8') as f:
+        json.dump(d, f)
+
+    d = get_ids_from_name(DATA_CARDS_FILE, DATA_LOC_FILE)
+    with open(path.join(data_folder, 'card_name_by_id.json'), 'w', encoding='utf-8') as f:
+        json.dump(d, f)
+
